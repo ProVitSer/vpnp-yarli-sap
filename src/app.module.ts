1baa07e8f4b4929daa33361acec6729a75c3869f
ProVitSer <prokinvs@gmail.com>
2022-08-30 22:33:31 +0300

    need modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AsteriskModule } from './asterisk/asterisk.module';
 import { LoggerModule } from './logger/logger.module';
 import { OrmModule } from './orm/orm.module';
 import { MicroserviceModule } from './microservice/microservice.module';
+import { CdrModule } from './cdr/cdr.module';
 import configuration from './config/config.provider';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] }), AsteriskModule, LoggerModule, MicroserviceModule],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot({ load: [configuration] }), AsteriskModule, LoggerModule, MicroserviceModule, OrmModule, CdrModule],
+  controllers: [AppController],
+  providers: [AppService],
   exports:[ConfigModule]
 })
 export class AppModule {}
