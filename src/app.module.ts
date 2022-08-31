9344d65e3571858a4bef042bd5de7f836dbab854
ProVitSer <prokinvs@gmail.com>
2022-08-31 21:29:11 +0300

    new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,11 @@ import { CdrModule } from './cdr/cdr.module';
 import configuration from './config/config.provider';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DockerModule } from './docker/docker.module';
+import { SeleniumModule } from './selenium/selenium.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] }), AsteriskModule, LoggerModule, MicroserviceModule, OrmModule, CdrModule],
+  imports: [ConfigModule.forRoot({ load: [configuration] }), AsteriskModule, LoggerModule, MicroserviceModule, OrmModule, CdrModule, DockerModule, SeleniumModule],
   controllers: [AppController],
   providers: [AppService],
   exports:[ConfigModule]
