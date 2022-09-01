4f613b7f8b04ce78828f97942ca4596572786927
ProVitSer <prokinvs@gmail.com>
2022-09-01 22:33:13 +0300

    docker service check

---
+++ src/health/health-indicator/docker.service.healthIndicator.ts
@@ -0,0 +1,47 @@
+import { DockerService } from '@app/docker/docker.service';
+import { Injectable } from '@nestjs/common';
+import { HealthIndicator, HealthIndicatorResult, HealthCheckError } from '@nestjs/terminus';
+
+
+@Injectable()
+export class DockerServiceHealthIndicator extends HealthIndicator {
+  constructor(
+    private readonly docker: DockerService
+  ) {
+    super();
+  }
+ 
+  async isHealthy(key: string): Promise<HealthIndicatorResult> {
+    try {
+      await this.docker.checkDocker();
+      return this.getStatus(key, true);
+    } catch (e) {
+      throw new HealthCheckError(
+        `${key} failed`,
+        this.getStatus(key, false)
+      );
+    }
+  }
+}
+
+@Injectable()
+export class DockerImgServiceHealthIndicator extends HealthIndicator {
+  constructor(
+    private readonly docker: DockerService
+  ) {
+    super();
+  }
+ 
+  async isHealthy(img: string, key: string): Promise<HealthIndicatorResult> {
+    try {
+      const result = await this.docker.checkImgRunning(img);
+      if(!result) throw `Нужный img: ${img} не запущен`
+      return this.getStatus(key, true);
+    } catch (e) {
+      throw new HealthCheckError(
+        `${key} failed`,
+        this.getStatus(key, false)
+      );
+    }
+  }
+}
